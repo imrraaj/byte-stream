cb71ead51b3a4ee435e6cb1a607c243971992299
rajkp <rajkpatel1728@gmail.com>
2025-04-11 18:42:17 +0530

    ADD: tinyfiledialogs library for cross platform file dialog

--- src/main.c
+++ src/main.c
@@ -1,32 +1,39 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <ctype.h>
-#include <string.h>
-#include "raylib.h"
-#include "player.h"
 #include "decoder.h"
+#include "player.h"
+#include "tinyfiledialogs.h"
+#include "raylib.h"
+
+#include <stdio.h>
 
 int main(int argc, char **argv)
 {
-    if (argc < 2)
+  char *file_name;
+  if (argc < 2)
+  {
+    char const *lFilterPatterns[] = {"*.mp4"};
+    file_name = tinyfd_openFileDialog("Please select a video file to play", ".", 1, lFilterPatterns, NULL, 1);
+    if (!file_name)
     {
-        fprintf(stderr, "Video file not provided\n");
-        fprintf(stderr, "USAGE: %s <file>\n", argv[0]);
-        exit(1);
+      tinyfd_messageBox("Error", "Invalid file selected", "ok", "error", 0);
+      return 1;
     }
+  }
+  else
+  {
+    file_name = argv[1];
+  }
 
-    char *file_name = argv[1];
-    if (player_init(file_name) < 0)
-    {
-        fprintf(stderr, "ERROR: Could not initialize player\n");
-        return -1;
-    }
+  if (player_init(file_name) < 0)
+  {
+    fprintf(stderr, "ERROR: Could not initialize player\n");
+    return -1;
+  }
 
-    while (!WindowShouldClose())
-    {
-        player_update();
-    }
+  while (!WindowShouldClose())
+  {
+    player_update();
+  }
 
-    player_close();
-    return 0;
+  player_close();
+  return 0;
 }
